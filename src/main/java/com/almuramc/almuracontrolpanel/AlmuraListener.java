b8471d5ca179eae75d626ef1e972ffe5120cb62a
Mike Howe <mike@mcsnetworks.com>
2014-07-08 19:30:31 -0500

    Update to make this better?!?
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/main/java/com/almuramc/almuracontrolpanel/AlmuraListener.java
+++ src/main/java/com/almuramc/almuracontrolpanel/AlmuraListener.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Almura Control Panel.
+ *
+ * © 2013 AlmuraDev <http://www.almuradev.com/>
+ * Almura Control Panel is licensed under the GNU General Public License.
+ *
+ * Almura Control Panel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Almura Control Panel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License. If not,
+ * see <http://www.gnu.org/licenses/> for the GNU General Public License.
+ */
 package com.almuramc.almuracontrolpanel;
 
 import org.bukkit.Material;
